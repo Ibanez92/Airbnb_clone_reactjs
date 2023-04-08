8f973b577491ecccae5a1993f83c41883257984b
Javier Ibanez <Expertprep2015@gmail.com>
2023-04-07 23:51:25 -0400

    added style to my hero section elements. Giving it a display of flex and a flex direction to column

--- src/assets/components/hero.jsx
+++ src/assets/components/hero.jsx
@@ -1,9 +1,7 @@
-export default function Hero(){
-    return(
-        <section>
-            <img src="src\assets\images\Group 77.png" className="hero--photo"/>
-            <h1 className="hero--header">Online Experiences</h1>
-            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home</p>
-        </section>
-            )
+export default function Hero() {
+    return (<section className="hero">
+        <img src="src\assets\images\Group 77.png" className="hero--photo"/>
+        <h1 className="hero--header">Online Experiences</h1>
+        <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home</p>
+    </section>)
 }
