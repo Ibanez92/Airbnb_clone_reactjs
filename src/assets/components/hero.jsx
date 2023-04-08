3011fbbbcf8be2c312f04a2618377d82ef25a7ab
Javier Ibanez <Expertprep2015@gmail.com>
2023-04-07 23:41:43 -0400

    added class names to my elements in the hero file to style them in the index.css file

--- src/assets/components/hero.jsx
+++ src/assets/components/hero.jsx
@@ -1,9 +1,9 @@
 export default function Hero(){
     return(
         <section>
-            <img src="src\assets\images\Group 77.png" />
-            <h1>Online Experiences</h1>
-            <p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home</p>
+            <img src="src\assets\images\Group 77.png" className="hero--photo"/>
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home</p>
         </section>
             )
 }
