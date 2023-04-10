3fbb403d07cedd2efa0c15d1d6e5e070e368ac0c
Javier Ibanez <Expertprep2015@gmail.com>
2023-04-09 23:08:43 -0400

    added a new div element for my with a class name of card--stats, added my star image with three span elements and two p elements

--- src/assets/components/card.jsx
+++ src/assets/components/card.jsx
@@ -1,7 +1,14 @@
 export default function Card() {
     return (
         <div className="card">
-            <img src="src\assets\images\image 12.png" className="card--image"/>
+            <img src="src\assets\images\image 12.png" alt="katie zaferes image" className="card--image"/>
+            <div className="card--stats">
+                <img src="src\assets\images\Star 1.png"/>
+                <span>5.0</span>
+                <span>(6) •</span>
+                <span>USA</span>
+            </div>
+            <p>Life Lesson with Katie Zaferes</p>
         </div>
     )
 }
