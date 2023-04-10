9c9c03dc403c8df56163c62dabb2eb01018b0e82
Javier Ibanez <Expertprep2015@gmail.com>
2023-04-09 23:26:13 -0400

    added a class name to my first two span elements in the card component and changed the color grey. Made the star a more approiate size

--- src/assets/components/card.jsx
+++ src/assets/components/card.jsx
@@ -3,13 +3,13 @@ export default function Card() {
         <div className="card">
             <img src="src\assets\images\image 12.png" alt="katie zaferes image" className="card--image"/>
             <div className="card--stats">
-                <img src="src\assets\images\Star 1.png"/>
-                <span>5.0</span>
-                <span>(6) •</span>
+                <img src="src\assets\images\Star 1.png" className="card--star"/>
+                <span className="grey">5.0</span>
+                <span className="grey">(6) •</span>
                 <span>USA</span>
             </div>
             <p>Life Lesson with Katie Zaferes</p>
-            <p>From $136 / person</p>
+            <p>From $136 / personz</p>
         </div>
     )
 }
