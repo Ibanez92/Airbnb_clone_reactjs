4b1a74d5901283a9441ef2da1d29d3f9b4affd29
Javier Ibanez <Expertprep2015@gmail.com>
2023-04-09 23:28:31 -0400

    rearranged the class names in the span to give a better UI

--- src/assets/components/card.jsx
+++ src/assets/components/card.jsx
@@ -4,9 +4,9 @@ export default function Card() {
             <img src="src\assets\images\image 12.png" alt="katie zaferes image" className="card--image"/>
             <div className="card--stats">
                 <img src="src\assets\images\Star 1.png" className="card--star"/>
-                <span className="grey">5.0</span>
+                <span>5.0</span>
                 <span className="grey">(6) •</span>
-                <span>USA</span>
+                <span className="grey">USA</span>
             </div>
             <p>Life Lesson with Katie Zaferes</p>
             <p>From $136 / personz</p>
