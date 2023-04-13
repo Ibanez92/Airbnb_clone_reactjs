7eefbc0ad3bddd7b6d2586290224807b1ee4a162
Javier Ibanez <Expertprep2015@gmail.com>
2023-04-12 23:32:20 -0400

    added rating and reviewCount props into my card component

--- src/assets/components/card.jsx
+++ src/assets/components/card.jsx
@@ -4,8 +4,8 @@ export default function Card(props) {
             <img src={`src/assets/images/${props.img}`} alt="katie zaferes image" className="card--image"/>
             <div className="card--stats">
                 <img src="src\assets\images\star_1.png" className="card--star"/>
-                <span>5.0</span>
-                <span className="grey">(6) •</span>
+                <span>{props.rating}</span>
+                <span className="grey">{props.reviewCount} •</span>
                 <span className="grey">USA</span>
             </div>
             <p>Life Lesson with Katie Zaferes</p>
