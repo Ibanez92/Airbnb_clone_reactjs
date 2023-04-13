1cfcbd35a2980234535ca4c323a41c0bd671c7ed
Javier Ibanez <Expertprep2015@gmail.com>
2023-04-12 23:34:23 -0400

    dded country prop into my card component

--- src/assets/components/card.jsx
+++ src/assets/components/card.jsx
@@ -6,7 +6,7 @@ export default function Card(props) {
                 <img src="src\assets\images\star_1.png" className="card--star"/>
                 <span>{props.rating}</span>
                 <span className="grey">{props.reviewCount} •</span>
-                <span className="grey">USA</span>
+                <span className="grey">{props.country}</span>
             </div>
             <p>Life Lesson with Katie Zaferes</p>
             <p><span className="bold">From $136</span> / person</p>
