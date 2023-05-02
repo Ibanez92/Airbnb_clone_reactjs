8158984702acf6890eda8b3984949a61fccf8a9d
Javier Ibanez <Expertprep2015@gmail.com>
2023-05-02 13:57:03 -0400

    fixed image paths

--- src/assets/components/card.jsx
+++ src/assets/components/card.jsx
@@ -4,15 +4,15 @@ import star from "../../images/star_1.png"
 export default function Card(props) {
     return (
         <div className="card">
-            <img src={props.img} alt="katie zaferes image" className="card--image"/>
+            <img src={props.img} className="card--image"/>
             <div className="card--stats">
                 <img src={star} className="card--star"/>
                 <span>{props.rating}</span>
                 <span className="grey">({props.reviewCount}) • </span>
                 <span className="grey">{props.country}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
