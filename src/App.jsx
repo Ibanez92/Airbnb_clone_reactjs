860cf35551da90e8081c56777a5d425cff1dc0e1
Javier Ibanez <Expertprep2015@gmail.com>
2023-05-01 22:37:05 -0400

    added image path to the db

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import Navbar from "./assets/components/navbar"
 import Hero from "./assets/components/hero"
 import Card from "./assets/components/card"
-import katie from "../src/images/katie_z.png"
 import data from "../data"
 
 function App() {
     const cards = data.map((info) => {
-        return <Card img={info.coverImg}
+        return ( <Card 
+            img={info.coverImg}
             rating={info.stats.rating}
             reviewCount={info.stats.reviewCount}
             country={info.location}
             title= {info.title}
-            price={info.price}/>
+            price={info.price}
+            /> 
+        )
     })
     return (
         <div>
