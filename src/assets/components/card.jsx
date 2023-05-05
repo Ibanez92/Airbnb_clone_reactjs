82875df0a343839743c6c5b3e487e1419aa684a6
Javier Ibanez <Expertprep2015@gmail.com>
2023-05-05 00:29:04 -0400

    added the info prop to my current card component jsx file

--- src/assets/components/card.jsx
+++ src/assets/components/card.jsx
@@ -12,15 +12,15 @@ export default function Card(props) {
     return (
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={props.img} className="card--image"/>
+            <img src={props.info.coverImg} className="card--image"/>
             <div className="card--stats">
                 <img src={star} className="card--star"/>
                 <span>{props.rating}</span>
-                <span className="grey">({props.reviewCount}) • </span>
-                <span className="grey">{props.country}</span>
+                <span className="grey">({props.info.reviewCount}) • </span>
+                <span className="grey">{props.info.location}</span>
             </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.info.title}</p>
+            <p className="card--price"><span className="bold">From ${props.info.price}</span> / person</p>
         </div>
     )
 }
